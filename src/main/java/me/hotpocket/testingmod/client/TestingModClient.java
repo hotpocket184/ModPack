809513e5532bf0008f5fd8cb5f5a4177ece9c052
hotpocket184 <80357042+hotpocket184@users.noreply.github.com>
2022-03-11 19:31:50 -0500

    Toggle Hacks

--- src/main/java/me/hotpocket/testingmod/client/TestingModClient.java
+++ src/main/java/me/hotpocket/testingmod/client/TestingModClient.java
@@ -3,13 +3,44 @@ package me.hotpocket.testingmod.client;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.LiteralText;
+import org.lwjgl.glfw.GLFW;
 
 @Environment(EnvType.CLIENT)
 public class TestingModClient implements ClientModInitializer{
+    public static TestingModClient instance;
+    private static KeyBinding keyBinding;
+    private static KeyBinding keyBinding2;
+    public boolean alwaysGround = false;
+    public boolean fastSpeed = false;
     @Override
     public void onInitializeClient() {
-
+        instance = this;
+        keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "Toggle Always Ground", // The translation key of the keybinding's name
+                InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+                GLFW.GLFW_KEY_R, // The keycode of the key
+                "Honeycomb Client" // The translation key of the keybinding's category.
+        ));
+        keyBinding2 = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "Toggle Fast Speed", // The translation key of the keybinding's name
+                InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+                GLFW.GLFW_KEY_K, // The keycode of the key
+                "Honeycomb Client" // The translation key of the keybinding's category.
+        ));
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (keyBinding.wasPressed()) {
+                alwaysGround = !alwaysGround;
+                client.player.sendMessage(new LiteralText("Toggled Always Ground - " + alwaysGround), true);
+            }
+            while (keyBinding2.wasPressed()) {
+                fastSpeed = !fastSpeed;
+                client.player.sendMessage(new LiteralText("Toggled Fast Speed - " + fastSpeed), true);
+            }
+        });
     }
-
-
 }
--- src/main/java/me/hotpocket/testingmod/mixin/TestingModMixin2.java
+++ src/main/java/me/hotpocket/testingmod/mixin/TestingModMixin2.java
@@ -1,7 +1,9 @@
 package me.hotpocket.testingmod.mixin;
 
+import me.hotpocket.testingmod.client.TestingModClient;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.fluid.WaterFluid;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.GameMode;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,8 +15,40 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class TestingModMixin2 {
 
     @Inject(at = @At("RETURN"), method = "move")
-    private void onDamage(CallbackInfo ci) {
+    private void onMove(CallbackInfo ci) {
         ClientPlayerEntity self = (ClientPlayerEntity) (Object) this;
-        self.setOnGround(true);
+        if(TestingModClient.instance.alwaysGround) {
+            self.setOnGround(true);
+        }
+        if(TestingModClient.instance.fastSpeed) {
+            if (self.isSneaking()
+                    || self.forwardSpeed == 0 && self.sidewaysSpeed == 0)
+                return;
+
+            // activate sprint if walking forward
+            if (self.forwardSpeed > 0 && !self.horizontalCollision)
+                self.setSprinting(true);
+
+            // activate mini jump if on ground
+            if (!self.isOnGround())
+                return;
+
+            Vec3d v = self.getVelocity();
+            self.setVelocity(v.x * 1.8, v.y + 0.1, v.z * 1.8);
+
+            v = self.getVelocity();
+            double currentSpeed = Math.sqrt(Math.pow(v.x, 2) + Math.pow(v.z, 2));
+
+            // limit speed to highest value that works on NoCheat+ version
+            // 3.13.0-BETA-sMD5NET-b878
+            // UPDATE: Patched in NoCheat+ version 3.13.2-SNAPSHOT-sMD5NET-b888
+            double maxSpeed = 0.66F;
+
+            if (currentSpeed > maxSpeed)
+                self.setVelocity(v.x / currentSpeed * maxSpeed, v.y,
+                        v.z / currentSpeed * maxSpeed);
+        }
     }
 }
+
+//langoria
